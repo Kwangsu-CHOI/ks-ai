5da848fffda1345d6599bc7a3d93391acd6500e0
Kwangsu-CHOI <wisecks@gmail.com>
2024-08-07 18:46:21 +1000

    index2

--- backend/index.js
+++ backend/index.js
@@ -182,11 +182,11 @@ app.use((err, req, res, next) => {
 	res.status(401).send("Unauthenticated!");
 });
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
 
 app.get("/", (req, res) => {
 	res.status(200).json({ message: "It is running." });
